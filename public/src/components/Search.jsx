eb23e7ee78de66e49d021d00ec39cf489c4434bd
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 16:38:21 -0700

    Set up search on change

--- public/src/components/Search.jsx
+++ public/src/components/Search.jsx
@@ -4,13 +4,18 @@ export default class Search extends Component {
 
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    console.log('hi');
   }
 
   render() {
     return (
       <div>
         <form>
-          <input type="text" name={this.props.type} placeholder={this.props.type}></input>
+          <input type="text" name={this.props.type} placeholder={this.props.type} onChange={this.handleChange}></input>
         </form>
       </div>
     );
