54dedc9b4b1ddca1cd422733882b62f49f2bb8c9
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 17:07:18 -0700

    Fix params for axios function

--- public/src/lib/searchSpotify.jsx
+++ public/src/lib/searchSpotify.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const searchSpotify = (type, query) => {
-  data = {
+  let data = {
     q: query,
     type: type,
     limit: 50
   };
-  axios.get('https://api.spotify.com/v1/search', data)
+  axios.get('https://api.spotify.com/v1/search', {params: data})
   .then((results) => {
     console.log('Success!');
     console.log(results);
