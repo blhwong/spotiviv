bd7226fe44b7ee465f41c1d19c9d7a2df8be12a4
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 22:41:51 -0700

    Supports tracks. Cleaned up code

--- public/src/components/ResultListEntry.jsx
+++ public/src/components/ResultListEntry.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 const ResultListEntry = (props) => {
   console.log(props);
+  let img;
+  if (props.type === 'Tracks') {
+    img = props.entry.album.images[2].url;
+  } else {
+    img = props.entry.images[2] ? props.entry.images[2].url : null;
+  }
+  let url = props.entry.external_urls.spotify;
+  let name = props.entry.name;
+  let popularity = props.entry.popularity;
   return (
     <div>
-      <img src={props.entry.images[2] ? props.entry.images[2].url : null}></img>
+      <img src={img}></img>
       <div>
-        <a href={props.entry.href}>{props.entry.name}</a>
-        {props.entry.popularity && <div>Popularity: {props.entry.popularity}</div>}
+        <a href={url}>{name}</a>
+        {popularity && <div>Popularity: {popularity}</div>}
       </div>
-
     </div>
   );
 };
