fe1b79d7bca821bac5cba4759e2463c6dbe13ac5
Brandon Wong <b.lh.wong@gmail.com>
2017-05-25 00:07:06 -0700

    Turn into a React Component

--- public/src/components/ResultList.jsx
+++ public/src/components/ResultList.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ResultListEntry from './ResultListEntry.jsx';
 
-const ResultList = (props) => {
-  console.log(props);
-  let entries = props.entries.artists || props.entries.albums || props.entries.tracks || null;
-  let type = props.entries.artists ? 'Artists' : (props.entries.albums ? 'Albums' : 'Tracks');
-  return (
-    <div>
-      <h2>{entries && 'Result List'}</h2>
-      <span>
-        <h3>{entries && type}{entries && type !== 'Albums' && <button>Sort by popularity: {props.ascending ? 'Descending' : 'Ascending'}</button>}</h3>
+export default class ResultList extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('click');
+    this.props.sortEntries();
+  }
+
+  render() {
+    console.log(this.props);
+    let entries = this.props.entries.artists || this.props.entries.albums || this.props.entries.tracks || null;
+    let type = this.props.entries.artists ? 'Artists' : (this.props.entries.albums ? 'Albums' : 'Tracks');
+    return (
+      <div>
+        <h2>{entries && 'Result List'}</h2>
+        <span>
+          <h3>{entries && type}{entries && type !== 'Albums' && <button onClick={this.handleClick}>Sort by popularity: {this.props.ascending ? 'Descending' : 'Ascending'}</button>}</h3>
 
 
-      </span>
-      {entries && entries.items.map((entry, index) => {
-        return (
-          <ResultListEntry
-            type={type}
-            entry={entry}
-            key={index}
-          />
-        );
-      })}
-    </div>
-  );
-};
+        </span>
+        {entries && entries.items.map((entry, index) => {
+          return (
+            <ResultListEntry
+              type={type}
+              entry={entry}
+              key={index}
+            />
+          );
+        })}
+      </div>
+    );
 
-export default ResultList;
+  }
+}
