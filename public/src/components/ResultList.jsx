03a543ec64193dbb730c7f8e36c87f4482fd98a1
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 22:41:25 -0700

    Supports tracks

--- public/src/components/ResultList.jsx
+++ public/src/components/ResultList.jsx
@@ -3,12 +3,12 @@ import ResultListEntry from './ResultListEntry.jsx';
 
 const ResultList = (props) => {
   console.log(props);
-  let entries = props.entries.artists || props.entries.albums || null;
-  let type = props.entries.artists ? 'Artists' : 'Albums';
+  let entries = props.entries.artists || props.entries.albums || props.entries.tracks || null;
+  let type = props.entries.artists ? 'Artists' : (props.entries.albums ? 'Albums' : 'Tracks');
   return (
     <div>
-      <h2>Result List</h2>
-      <h3>{type}</h3>
+      <h2>{entries && 'Result List'}</h2>
+      <h3>{entries && type}</h3>
       {entries && entries.items.map((entry, index) => {
         return (
           <ResultListEntry
