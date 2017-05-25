57b84e735afc73183197459916ee85a6119845fb
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 21:18:14 -0700

    Changed array to object for entries

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      entries: []
+      entries: {}
     };
     this.setEntries = this.setEntries.bind(this);
   }
