4cf8aaf99e77edcff50851704b7cacecb4763c2d
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 22:06:31 -0700

    Capitalization

--- public/src/components/ResultList.jsx
+++ public/src/components/ResultList.jsx
@@ -4,10 +4,11 @@ import ResultListEntry from './ResultListEntry.jsx';
 const ResultList = (props) => {
   console.log(props);
   let entries = props.entries.artists || props.entries.albums || null;
-  let type = props.entries.artists ? 'artists' : 'albums';
+  let type = props.entries.artists ? 'Artists' : 'Albums';
   return (
     <div>
       <h2>Result List</h2>
+      <h3>{type}</h3>
       {entries && entries.items.map((entry, index) => {
         return (
           <ResultListEntry
