97fda474dfb27ea43048c25c9a1bc4bace3252ad
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 21:59:45 -0700

    Renders artists

--- public/src/components/ResultListEntry.jsx
+++ public/src/components/ResultListEntry.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const ResultListEntry = (entry) => {
+const ResultListEntry = (props) => {
+  console.log(props);
+  if (props.type === 'artists') {
+    return (
+      <div>
 
+        <img src={props.entry.images[2] ? props.entry.images[2].url : null}></img>
+        <div>
+          <a href={props.entry.href}>{props.entry.name}</a>
+          <div>Popularity: {props.entry.popularity}</div>
+        </div>
+
+      </div>
+    );
+  } else {
+
+  }
+  return (
+    null
+  );
 };
 
 export default ResultListEntry;
