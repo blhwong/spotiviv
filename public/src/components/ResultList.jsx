871306561608a8ab867e37443898989e52961e75
Brandon Wong <b.lh.wong@gmail.com>
2017-05-24 21:39:21 -0700

    Maps out result entries to entrylist component

--- public/src/components/ResultList.jsx
+++ public/src/components/ResultList.jsx
@@ -3,8 +3,21 @@ import ResultListEntry from './ResultListEntry.jsx';
 
 const ResultList = (props) => {
   console.log(props);
+  let entries = props.entries.artists || props.entries.albums || null;
+  let type = props.entries.artists ? 'artists' : 'albums';
   return (
-    <div>ResultList</div>
+    <div>
+      <h2>Result List</h2>
+      {entries && entries.items.map((entry, index) => {
+        return (
+          <ResultListEntry
+            type={type}
+            entry={entry}
+            key={index}
+          />
+        );
+      })}
+    </div>
   );
 };
 
